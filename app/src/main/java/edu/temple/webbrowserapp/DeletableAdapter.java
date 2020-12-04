16af3027f8770cf07f631e8c45d104a929564f25
alexlin1822 <alexlin1822@hotmail.com>
2020-12-04 15:23:25 -0500

    Ass 10 Init

--- app/src/main/java/edu/temple/webbrowserapp/DeletableAdapter.java
+++ app/src/main/java/edu/temple/webbrowserapp/DeletableAdapter.java
@@ -1,7 +1,10 @@
 package edu.temple.webbrowserapp;
 
 import java.util.ArrayList;
+
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -63,9 +66,25 @@ public class DeletableAdapter extends BaseAdapter{
 
             @Override
             public void onClick(View v) {
-                text.remove(index);
-                mAttentionClickListener.DeleteItem(index);
-                notifyDataSetChanged();
+                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                builder.setMessage("Delete"+" "+text.get(index)+"?");
+                builder.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        text.remove(index);
+                        mAttentionClickListener.DeleteItem(index);
+                        notifyDataSetChanged();
+                    }
+                });
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
             }
         });
         return view;
